6da7d159707ecb5f3ed7f7bc7ec596155d3729ee
fancydevpro <borisglab@gmail.com>
2018-11-19 16:52:08 +0200

    import custome module

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var dt = require('./datetime');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('The date and time are currently: ' + dt.myDateTime());
   res.end('Hello World!');
 }).listen(8080);
