0ee5d0e3e47cff353b569e7189f785f4b84f02f8
fancydevpro <borisglab@gmail.com>
2018-11-19 17:36:02 +0200

    delete file

--- writefile.js
+++ writefile.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.writeFile('temp/mynewfile3.txt', 'Hello content!', function (err) {
+fs.writeFile('temp/mynewfile3.txt', 'This is my text', function (err) {
   if (err) throw err;
-  console.log('Saved!');
+  console.log('Replaced!');
 });
