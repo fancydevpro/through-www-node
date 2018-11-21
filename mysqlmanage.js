4e428437d1aee6735af6be5de2873f3b20c5b3b0
fancydevpro <borisglab@gmail.com>
2018-11-21 20:31:22 +0200

    create db

--- mysqlmanage.js
+++ mysqlmanage.js
@@ -10,4 +10,11 @@ var con = mysql.createConnection({
 con.connect(function (err) {
   if (err) throw err
   console.log('Connected!')
+  con.query('CREATE DATABASE rhs_db', function (err, result) {
+    if (err) {
+      console.log('Create Error: ', err)
+      throw err
+    }
+    console.log('Result: ', result)
+  })
 })
